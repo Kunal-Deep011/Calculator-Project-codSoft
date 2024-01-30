2778c40ad561d7024fe42e57238bbbb7a3004fdd
Kunal Deep <kunalsah3332@gmail.com>
2024-01-30 17:04:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let input = document.getElementById("input-box");
+let buttons = document.querySelectorAll('button');
+const specialChars = ["%", "*", "/", "-", "+", "="];
+let output ="";
+
+const calculate =(btnValue) =>{
+      if(btnValue === "=" && output !==""){
+
+        output = eval(output.replace("%", "/100"));
+      }else if(btnValue === "AC"){
+        output = "";
+      }else if(btnValue === "DEL"){
+         output = output.toString().slice(0,-1);
+      }else{
+        if(output === "" && specialChars.includes(btnValue)) return;
+        output += btnValue;
+      }
+      input.value = output;
+}
+
+buttons.forEach((button) =>{
+    button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+})
